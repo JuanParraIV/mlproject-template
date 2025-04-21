9ca6535ace15120f024ba9bb8fed9fe43374795e
JuanParraIV <jmparra1993@gmail.com>
2025-04-21 16:51:58 -0500

    Add time_left.tsv to track iteration progress and remaining time for CatBoost training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,6 +12,10 @@ from src.components.data_transformation import (
     DataTransformation,
     DataTransformationConfig,
 )
+from src.components.model_trainer import (
+    ModelTrainer,
+    ModelTrainerConfig,
+)
 
 
 @dataclass
@@ -56,11 +60,11 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    train_data, test_data =obj.initiate_data_ingestion()
-    
+    train_data, test_data = obj.initiate_data_ingestion()
+
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(
         train_data, test_data
     )
-    
-    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
