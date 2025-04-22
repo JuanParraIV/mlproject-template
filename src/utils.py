46bcf1527c8b508cccbc7b94b33b4e7d9b8b56fb
JuanParraIV <jmparra1993@gmail.com>
2025-04-21 19:17:44 -0500

    Implement prediction pipeline and web interface for student exam performance prediction

--- src/utils.py
+++ src/utils.py
@@ -21,6 +21,12 @@ def save_object(file_path: str, obj: str):
     except Exception as e:
         raise CustomException(e, sys)
 
+def load_object(file_path: str):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
 
 def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
